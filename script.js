1dacb61afe03f11b1c10ca739c31a390833e2ed7
Amrit2608 <74068336+Amrit2608@users.noreply.github.com>
2023-12-14 18:33:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // script.js
 
-function revealMessage() {
-   var hiddenMessage = document.getElementById("hidden-message");
-   hiddenMessage.style.display = "block";
+
+
+
+
+function revealMessageAndVideo() {
+  // Show the hidden message
+  var hiddenMessage = document.getElementById("hidden-message");
+  hiddenMessage.style.display = "block";
+
+  // Show the video
+  var birthdayVideo = document.getElementById("birthday-video");
+  birthdayVideo.style.display = "block";
 }
